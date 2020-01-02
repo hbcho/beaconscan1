599efb476043742037dc296667400fb3259894e4
hbcho <52642437+hbcho@users.noreply.github.com>
2020-01-02 16:13:15 +0900

    Update blescan.py

--- blescan.py
+++ blescan.py
@@ -161,14 +161,14 @@ def parse_events(sock, loop_count=100):
                     	rssi, = struct.unpack("b", pkt[report_pkt_offset -1])
                     	print "\tRSSI:", rssi
 		    # build the return string
-                    #Adstring = packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
-		    #Adstring += ","
+                    Adstring = packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
+		    Adstring += ","
 		    #Adstring += returnstringpacket(pkt[report_pkt_offset -22: report_pkt_offset - 6]) 
 		    #Adstring += ","
-		    #Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -6: report_pkt_offset - 4]) 
-		    #Adstring += ","
-		    #Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -4: report_pkt_offset - 2])
-		    #Adstring += ","
+		    Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -6: report_pkt_offset - 4]) 
+		    Adstring += ","
+		    Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -4: report_pkt_offset - 2])
+		    Adstring += ","
 		    #Adstring += "%i" % struct.unpack("b", pkt[report_pkt_offset -2])
 		    #Adstring += ","
 		    #Adstring += "%i" % struct.unpack("b", pkt[report_pkt_offset -1])
